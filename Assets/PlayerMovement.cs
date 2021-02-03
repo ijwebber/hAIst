eb7d9d8fc9b8855135db91da34d2d0e014b9d3ac
Isaac <isaacjwebber@gmail.com>
2021-02-03 21:59:59 +0000

    added player movement and also camera following

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 0.2f;
+
+    void Update() {
+        if (Input.GetKey("w")) {
+            this.transform.localPosition += new Vector3(0, 0, speed);
+        }
+        if (Input.GetKey("a")) {
+            this.transform.localPosition += new Vector3(-speed, 0, 0);
+        }
+        if (Input.GetKey("s")) {
+            this.transform.localPosition += new Vector3(0, 0, -speed);
+        }
+        if (Input.GetKey("d")) {
+            this.transform.localPosition += new Vector3(speed, 0, 0);
+        }
+        
+        
+    }
+}
