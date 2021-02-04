659189cb423a04b7a5bc2015d85259fbfd32b5ee
Isaac <isaacjwebber@gmail.com>
2021-02-04 12:23:19 +0000

    added basic rotation, needs fixing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,12 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5;
     private Rigidbody rb;
+    Vector3 m_EulerAngleVelocity;
  
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+
     }
  
     void FixedUpdate()
@@ -17,7 +19,11 @@ public class PlayerMovement : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         
         Vector3 moveVector = new Vector3(horizontal, 0, vertical);
+        Quaternion deltaRotation = Quaternion.LookRotation(moveVector);
+
         moveVector = moveVector.normalized * speed * Time.deltaTime;
         rb.MovePosition(transform.position + moveVector);
+        
+        rb.rotation = deltaRotation;
     }
 }
