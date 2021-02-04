62e3be6a446dc6f8a706ba33df727fe8b6abb220
Shahin <shahinshahrokh@rocketmail.com>
2021-02-04 15:51:42 +0000

    added wall transparency effect

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,13 +7,20 @@ public class FollowPlayer : MonoBehaviour
 
     public GameObject obstruction;  // global obstructions 
 
-    public string name;
+    public string name; // name of current object in view
     
+    public Material objectMaterial;
+
+    public Material prevObjectMaterial;
+
+    public string[] currentObject = {""};  // at most, there will probably be two walls in this array so initialise for two strings, but for now just do 1
+
 
     // Update is called once per frame
     void Update()
     {
         transform.position = player.position + offset;
+        
         
         Ray ray = GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
         RaycastHit hit;
@@ -24,33 +31,51 @@ public class FollowPlayer : MonoBehaviour
             print("I'm looking at " + hit.transform.name);
 
             obstruction = hit.transform.gameObject;
+            name = obstruction.name; // name of obstruction
+            objectMaterial = obstruction.GetComponent<Renderer>().material; 
             
-
-            if(obstruction.tag == "hideObject"){
-                
-                //objectHit.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
-                name = obstruction.name; // change name to previous wall 
-            
-                obstruction.GetComponent<Renderer>().enabled = false;
+            if(obstruction.tag == "hideObject" &&  currentObject[0] == name){   // case where we are hidden behind a wall but we don't move to another wall
+                //obstruction.GetComponent<Renderer>().enabled = false;  
+                SetAlpha(0.5F);     
             }
-
-
-            else if (obstruction.name == "Cube") {  
+            else if(obstruction.tag == "hideObject" &&  currentObject[0] == ""){ // case where we are hidden behind a wall but the array is "empty"
+                //obstruction.GetComponent<Renderer>().enabled = false;
+                SetAlpha(0.5F);  
+                currentObject[0] = name;
+            }
+            else if (obstruction.tag == "hideObject" &&  currentObject[0] != name){ // case where we are hidden behind a new wall to the one in the array
+                GameObject prev = GameObject.Find(currentObject[0]);   // change prev wall to visible
+                //prev.GetComponent<Renderer>().enabled = true; 
+                prevObjectMaterial = prev.GetComponent<Renderer>().material; 
+                SetAlpha1(1F); 
+                currentObject[0] = name; // replace with new name
+            }
+            else if (obstruction.name == "Timmy" && currentObject[0] != "") {  
             // set previous object back to visible, need to have a way to specifically change the visibility of the previous object
-
-                GameObject prev = GameObject.Find("wall-corner (9)");   // needs to be changed
-                prev.GetComponent<Renderer>().enabled = true; 
-                
-
+                GameObject prev = GameObject.Find(currentObject[0]);   
+                //prev.GetComponent<Renderer>().enabled = true; 
+                prevObjectMaterial = prev.GetComponent<Renderer>().material; 
+                SetAlpha1(1F); 
+                currentObject[0] = "";
             }
         }
-
-
         else
         {
             print("I'm looking at nothing ");
             
         }
         
+    }
+
+    void SetAlpha(float alpha){            // need to merge these together by creating maybe just one object material class?
+        Color color = objectMaterial.color;
+        color.a = alpha;
+        objectMaterial.color = color;
+    }
+
+    void SetAlpha1(float alpha){
+        Color color = prevObjectMaterial.color;
+        color.a = alpha;
+        prevObjectMaterial.color = color;
     }
 }
