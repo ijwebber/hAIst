47b7305dea298ebac303905b5189a1cc1cb084a2
Isaac <isaacjwebber@gmail.com>
2021-02-05 12:03:33 +0000

    added alarm light

---
+++ Assets/Scripts/AlarmLight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class AlarmLight : MonoBehaviour
+{
+    Light alarm;
+    public int speed = 5;
+    public float maxIntensity = 0.5f;
+    public float minIntensity = 0f;
+
+    void Start()
+    {
+        alarm = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        float diff = ((maxIntensity - minIntensity) / 2);
+
+        alarm.intensity =  diff * Mathf.Sin(speed * Time.time) + diff + minIntensity;
+    }
+}
