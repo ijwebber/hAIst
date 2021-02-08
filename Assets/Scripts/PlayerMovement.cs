bcddf3366e8863ce9909988e344091a2fcbd5144
Isaac <isaacjwebber@gmail.com>
2021-02-08 17:53:49 +0000

    fixed rotation and add wall layer to all walls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,23 +15,18 @@ public class PlayerMovement : MonoBehaviour
  
     void FixedUpdate()
     {
-      
-
-
         // player movement - forward, backward, left, right
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         
         Vector3 moveVector = new Vector3(horizontal, 0, vertical);
-        Quaternion deltaRotation = Quaternion.LookRotation(moveVector);
+
+        if (moveVector != Vector3.zero) {
+            Quaternion deltaRotation = Quaternion.LookRotation(moveVector);
+            rb.rotation = deltaRotation;
+        }
 
         moveVector = moveVector.normalized * speed * Time.deltaTime;
         rb.MovePosition(transform.position + moveVector);
-        
-        rb.rotation = deltaRotation;
     }
-
- 
-
-
 }
