b4d6f52317e47d31915bb394b3dc4d52032ee24e
Shahin <shahinshahrokh@rocketmail.com>
2021-02-09 21:26:43 +0000

    score system improved and fixed various bugs

--- Assets/Scripts/pickUp.cs
+++ Assets/Scripts/pickUp.cs
@@ -18,6 +18,10 @@ public class pickUp : MonoBehaviour
 
     private bool held = false;
 
+    private string[] paint1 = {"MonaLisa","TheShahin","TilosPride"};
+
+    private int[] scores = {250,600,1000};
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,7 +34,7 @@ public class pickUp : MonoBehaviour
     void Update()
     {
         
-        if(inRange){print("INRANGE");}      // keep track of whether player is in range or not
+        if(inRange){print(gameObject.name);}      // keep track of whether player is in range or not
         else{print("NOT IN RANGE");}
 
         int timeLeft = mainCam.GetComponent<FollowPlayer>().seconds;  // access the seconds variable from the mainCam class/ follow player script
@@ -40,8 +44,8 @@ public class pickUp : MonoBehaviour
             timer = startTime;
         }
 
-        if(Input.GetKey(KeyCode.E) && held == false){   // for each time E is being held down, count/increment the timer
-            timer += Time.deltaTime;
+        if(Input.GetKey(KeyCode.E) && held == false && timeLeft == 0){   // for each time E is being held down, count/increment the timer
+            timer += Time.deltaTime;                    // Needs fix so that if there's still time left, the user doesn't hold down E from 5 seconds for example
             if(timer>(startTime + holdTime)){   // if the time reaches 5s, then set held to true, else set to false
                 held = true;
             }
@@ -55,7 +59,27 @@ public class pickUp : MonoBehaviour
         }
 
         if(held && inRange && timeLeft == 0){ // if both player is in range and the button E is pressed, then add points to the score
-            mainCam.GetComponent<FollowPlayer>().points+=50;
+
+            string paintingName = gameObject.name;
+            int paintingIndex = 0;
+            bool exists = false;
+
+            for(int i = 0; i <= paint1.Length;i++){ // find if painting is part of the list etc...
+                if(paint1[i] == paintingName){
+                    exists = true;                 // if so, then set exists to true and set the index
+                    paintingIndex = i;
+                    break;
+                }
+            }
+
+            if(exists){
+                int cost = scores[paintingIndex];   // index of the painting matches the index of its score
+                mainCam.GetComponent<FollowPlayer>().points+=cost;  
+            }
+            else{
+                mainCam.GetComponent<FollowPlayer>().points+=10; // if the painting isn't "valuable", then just give it 10 points
+            }
+
             mainCam.GetComponent<FollowPlayer>().targetTime += 11;  // increase time duration
             Destroy(gameObject);
         }
