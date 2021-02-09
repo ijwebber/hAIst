fd1fb8b7cc5501043b38a2474b05e3c71df496f5
Daanial <Daanial74@gmail.com>
2021-02-09 17:17:53 +0000

    Field of view added
    Currently just on Conrad (fieldofView + fieldofViewEditor scripts). Any object that you want for it to track needs a tag "targets", and any obstacles that you want to block the fov with can be changed under the obstacle variable under the inspector for Conrad (currently set to walls). Currently it just draws a line which can be seen in the editor and debugprints (you can change this to do whatever).

---
+++ Assets/Editor/FieldOfViewEditiro.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (FieldOfView))]
+public class FieldOfViewEditiro : Editor
+{
+
+
+    //This script allows you to see a visual for radius and viewing angle size and it draws lines to the objects that are in the field of view
+    private void OnSceneGUI()
+    {
+        FieldOfView fov = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
+        Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2, false);
+        Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2, false);
+
+        //draws viewing radius and viewing angle.
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+
+        //draws a line to every target in visibleTargets in red using their positions
+        Handles.color = Color.red;
+        foreach(Transform visibleTarget in fov.visibleTargets)
+        {
+            Handles.DrawLine(fov.transform.position, visibleTarget.position);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
