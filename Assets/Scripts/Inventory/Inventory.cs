a0c8832e91398a4531cff09d72d57ee63d2f186a
Isaac <isaacjwebber@gmail.com>
2021-02-11 20:21:30 +0000

    added some inventory gui and logic

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+
+    public int size = 5;
+    private bool[] isFullList;
+
+    private GameObject[] items;
+
+    public GameObject[] slots;
+
+    public Text itemText;
+
+
+    void Start() {
+        isFullList = new bool[size];
+        items = new GameObject[size];
+    }
+
+    public void Add(GameObject item) {
+        for (int i = 0; i < items.Length; i++) {
+            if (isFullList[i] == false) {
+                items[i] = item;
+
+                item.transform.position = slots[i].transform.position; // Move object to the inventory camera setup
+                item.layer = 14;    // Update to the correct layer to be visible
+
+                isFullList[i] = true;
+
+                break;
+            }
+        }
+    }
+
+    public bool isFull() {
+        for (int i = 0; i < items.Length; i++) {
+            if (isFullList[i] == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void ShowName(int i) {
+        if (isFullList[i]) {
+            itemText.enabled = true;
+            itemText.text = items[i].name; 
+        }
+    }
+}
---
+++ Assets/Scripts/Inventory/InventoryHover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventoryHover : MonoBehaviour
+{
+    void OnPointerEnter()
+    {
+        Debug.Log(":Enterd");
+    }
+}
