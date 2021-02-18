b4b2acb0436fed0625c4e8b84064c26bd290dbf2
Isaac <isaacjwebber@gmail.com>
2021-02-18 11:17:53 +0000

    fixed flashlight

--- Assets/Scripts/Player/FlashlightSwitch.cs
+++ Assets/Scripts/Player/FlashlightSwitch.cs
@@ -1,27 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class FlashlightSwitch : MonoBehaviour
+public class FlashlightSwitch : MonoBehaviourPun
 {
-    Light flashlight;
+    public Light flashlight;
+    float initialIntensity;
 
-    void Start()
-    {
-        flashlight = GetComponent<Light>();
+    void Start() {
+        initialIntensity = flashlight.intensity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F)) {
-            bool isOn = flashlight.intensity == 1;
+        if (photonView.IsMine == true && PhotonNetwork.IsConnected == true)
+        {
+            if (Input.GetKeyDown(KeyCode.F)) {
+                bool isOn = flashlight.intensity == initialIntensity;
 
-            if (isOn) {
-                flashlight.intensity = 0;
-            } else {
-                flashlight.intensity = 1;
-            }
+                if (isOn) {
+                    flashlight.intensity = 0;
+                } else {
+                    flashlight.intensity = initialIntensity;
+                }
+            }  
         }        
     }
 }
