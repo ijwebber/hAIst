00ecfb056ef9935c55d5d8336d3ecfe40416f5ec
Daanial <Daanial74@gmail.com>
2021-02-19 19:00:13 +0000

    Guard Knock out from behind

---
+++ Assets/Scripts/Player/KnockOutGuard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class KnockOutGuard : MonoBehaviour
+{   
+
+    private bool inRangeOfGuard = false;
+    private int guardViewID = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRangeOfGuard && guardViewID != -1)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GuardMovement guardScript = PhotonNetwork.GetPhotonView(guardViewID).GetComponentInParent<GuardMovement>();
+                guardScript.guardDisabled = true;
+            }
+        }
+
+        inRangeOfGuard = false;
+        guardViewID = -1;
+    }
+
+
+    [PunRPC]
+    void KnockOut(int viewID)
+    {
+        inRangeOfGuard = true;
+        guardViewID = viewID;
+    }
+}
