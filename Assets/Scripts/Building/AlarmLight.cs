ab4bb1bdd8d6bb8c18374a8e98a3b0b0bf920ab2
Isaac Webber <isaacjwebber@gmail.com>
2021-02-25 13:03:12 +0000

    updated the alarm to have an on and off function

--- Assets/Scripts/Building/AlarmLight.cs
+++ Assets/Scripts/Building/AlarmLight.cs
@@ -5,17 +5,29 @@ public class AlarmLight : MonoBehaviour
     Light alarm;
     public int speed = 5;
     public float maxIntensity = 0.5f;
-    public float minIntensity = 0f;
+    public float minIntensity = 0.1f;
+    private bool lightOn = false;
 
     void Start()
     {
         alarm = GetComponent<Light>();
+        alarm.intensity = 0;
     }
 
     void Update()
     {
-        float diff = ((maxIntensity - minIntensity) / 2);
+        if (lightOn) {
+            float diff = ((maxIntensity - minIntensity) / 2);
+            alarm.intensity =  diff * Mathf.Sin(speed * Time.time) + diff + minIntensity;
+        }
+    }
 
-        alarm.intensity =  diff * Mathf.Sin(speed * Time.time) + diff + minIntensity;
+    public void On() {
+        lightOn = true;
+    }
+
+    public void Off() {
+        lightOn = false;
+        alarm.intensity = 0;
     }
 }
