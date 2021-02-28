2cde3abc46552277a28ea411c7ba22b57b27fa4c
Isaac Webber <isaacjwebber@gmail.com>
2021-02-28 18:22:29 +0000

    rotate task is now clockwise

--- Assets/Scripts/Tasks (Canvas)/RotateImage.cs
+++ Assets/Scripts/Tasks (Canvas)/RotateImage.cs
@@ -6,7 +6,7 @@ public class RotateImage : MonoBehaviour
 {
    public Image image;
    public void rotateImage(){
-        image.GetComponent<RectTransform>().Rotate(0f,0f,90f);      // rotate each image by 90 degrees
+        image.GetComponent<RectTransform>().Rotate(0f,0f,-90f);      // rotate each image by 90 degrees
     }                                                               // this is assigned to each image but can be integrated into the rotatetask script
   
 }
