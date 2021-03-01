23709fd92ebd91b793421253946f0c10ab06d730
Josh Felmeden <josh.felmeden@outlook.com>
2021-03-01 11:48:03 +0000

    added color to progress bar

--- Assets/Scripts/Player/ProgressBarController.cs
+++ Assets/Scripts/Player/ProgressBarController.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProgressBarController : MonoBehaviour
 {
     public GameObject front;
+    public Gradient gradient;
 
     public void UpdateBar(float x, float min, float max) {
         float width = (x / (max - min)) * 200;
         front.GetComponent<RectTransform>().sizeDelta = new Vector2 (width, 16); 
+        front.GetComponent<Image>().color = gradient.Evaluate(x/(max-min));
     }
 
     public void ResetBar() {
