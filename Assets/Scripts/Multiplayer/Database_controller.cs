20563595fec7cf5b9681ef0940d3d8803d65449a
Alex <brownalexoscar@gmail.com>
2021-03-03 18:40:19 +0200

    trying out database stuff

---
+++ Assets/Scripts/Multiplayer/Database_controller.cs
@@ -0,0 +1,44 @@
+﻿ using System;
+ using System.Data;
+ using MySql.Data.MySqlClient;
+ 
+ public class Test
+ {
+    public static void Main(string[] args)
+    {
+       string connectionString =
+          "Server=localhost;" +
+          "Database=test;" +
+          "User ID=myuserid;" +
+          "Password=mypassword;" +
+          "Pooling=false";
+       IDbConnection dbcon;
+       dbcon = new MySqlConnection(connectionString);
+       dbcon.Open();
+       IDbCommand dbcmd = dbcon.CreateCommand();
+       // requires a table to be created named employee
+       // with columns firstname and lastname
+       // such as,
+       //        CREATE TABLE employee (
+       //           firstname varchar(32),
+       //           lastname varchar(32));
+       string sql =
+           "SELECT firstname, lastname " +
+           "FROM employee";
+       dbcmd.CommandText = sql;
+       IDataReader reader = dbcmd.ExecuteReader();
+       while(reader.Read()) {
+            string FirstName = (string) reader["firstname"];
+            string LastName = (string) reader["lastname"];
+            Console.WriteLine("Name: " +
+                  FirstName + " " + LastName);
+       }
+       // clean up
+       reader.Close();
+       reader = null;
+       dbcmd.Dispose();
+       dbcmd = null;
+       dbcon.Close();
+       dbcon = null;
+    }
+ }
