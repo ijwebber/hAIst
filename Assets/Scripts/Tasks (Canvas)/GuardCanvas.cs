d10ff457d95e95f15d2fe43aa75cf7a01930aa96
Josh Felmeden <josh.felmeden@outlook.com>
2021-03-17 13:50:30 +0000

    pre change guard icon

---
+++ Assets/Scripts/Tasks (Canvas)/GuardCanvas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuardCanvas : MonoBehaviour
+{
+    public Sprite sus;
+    public Sprite exclamation;
+    public GuardController controller;
+    public Canvas canvas;
+    public List<Image> guardIndicators;
+    public Image exampleGuard;
+
+    private void Awake() {
+        guardIndicators = new List<Image>();
+        foreach (var guard in controller.guardMovements)
+        {
+            guardIndicators.Add(Instantiate<Image>(exampleGuard, this.transform));
+        }
+    }
+}
---
+++ Assets/Scripts/Tasks (Objects)/Window_QuestPointer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using UnityEngine.UI;
+
+public class Window_QuestPointer : MonoBehaviour
+{
+
+    public GameObject targetObject;
+    public Canvas canvas;
+    private Vector3 targetPosition;
+    public PlayerController playerController;
+    private RectTransform pointerRectTransform;
+    private Image pointerImage;
+    public Sprite arrowSprite;
+    public Sprite crossSprite;
+    void Awake()
+    {
+        // targetPosition = new Vector3(-39.615f,42.6f);
+        Debug.Log(targetPosition);
+        pointerRectTransform = transform.Find("Pointer").GetComponent<RectTransform>();
+        pointerImage = transform.Find("Pointer").GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        targetPosition = new Vector3(targetObject.transform.position.x, targetObject.transform.position.z);
+        Vector3 toPosition = targetPosition;
+        Vector3 fromPosition = new Vector3(GameObject.Find("Timmy").transform.position.x, GameObject.Find("Timmy").transform.position.z);
+        // fromPosition.y = 0;
+        Vector3 dir = (toPosition - fromPosition).normalized;
+        float angle = UtilsClass.GetAngleFromVectorFloat(dir);
+        pointerRectTransform.localEulerAngles = new Vector3(0,0,angle);
+        
+        Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(targetObject.transform.position);
+        bool isOffScreen = !playerController.isInView(targetObject.transform.position);
+
+        if (isOffScreen) {
+            pointerRectTransform.sizeDelta = new Vector2(30,20);
+            pointerImage.sprite = arrowSprite;
+            pointerRectTransform.localPosition = new Vector3(100*dir.x,100*dir.y);
+        } else {
+            pointerRectTransform.sizeDelta = new Vector2(20,45);
+            pointerRectTransform.localEulerAngles = new Vector3(0,0,0);
+            pointerImage.sprite = crossSprite;
+            pointerRectTransform.anchoredPosition = new Vector2(targetPositionScreenPoint.x - canvas.GetComponent<RectTransform>().position.x / canvas.scaleFactor, (targetPositionScreenPoint.y - canvas.GetComponent<RectTransform>().position.y) + 60);
+        }
+    }
+}
