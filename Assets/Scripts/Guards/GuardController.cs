305f3d5e628a34e679ef778a3860bbf8ce50178c
Josh Felmeden <josh.felmeden@outlook.com>
2021-03-17 16:07:12 +0000

    Revert "merged ui-canvas with master"
    This reverts commit b1221a3e2cbbb4d1c76f5d9af8306d72ca3a3878, reversing
    changes made to b0ddb4b3e35be0647e7d1d752aacb8788abb6465.

--- Assets/Scripts/Guards/GuardController.cs
+++ Assets/Scripts/Guards/GuardController.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
 
-public class GuardController : MonoBehaviourPun
+public class GuardController : MonoBehaviour
 {
     public LayerMask obstacleMask;
     public Grid localGrid;
-    public GuardMovement[] guardMovements;
+    public Sprite sus;
+    public Sprite exclamation;
+    private GuardMovement[] guardMovements;
     public PlayerController playerController;
 
     // Start is called before the first frame update
@@ -15,6 +16,7 @@ public class GuardController : MonoBehaviourPun
     {
         // this.localGrid = new Grid(202,122,.5f);
         this.localGrid = new Grid(202,122,1);
+        guardMovements = GameObject.FindObjectsOfType<GuardMovement>();
     }
 
     public void setValue(Vector3 position, float intensity) {
@@ -29,12 +31,30 @@ public class GuardController : MonoBehaviourPun
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (guardMovements.Length == 0) {
-            guardMovements = GameObject.FindObjectsOfType<GuardMovement>();
-        }
         localGrid.updateNodes();
     }
     
+    void Update() {
+        foreach (GuardMovement guard in guardMovements) {
+            // if (Physics.Raycast(guard.gameObject.transform.position, (playerController.player.transform.position - guard.gameObject.transform.position).normalized, playerController.viewRadius+2,obstacleMask)) {
+                switch (guard.state)
+                {
+                    case State.normal:
+                        guard.sprite.sprite = null;
+                        break;
+                    case State.suspicious:
+                        guard.sprite.sprite = sus;
+                        break;
+                    case State.chase:
+                        guard.sprite.sprite = exclamation;
+                        break;
+                }
+            // } else {
+            //     guard.sprite.sprite = null;
+            // }
+        }
+    }
+
     public bool inChase() {
         foreach (GuardMovement guard in guardMovements) {
             if (guard.state == State.chase) {
