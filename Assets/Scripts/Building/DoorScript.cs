221a0ff77c0e55c67c016299b56850ee49c685b4
Josh Felmeden <josh.felmeden@outlook.com>
2021-03-18 20:40:53 +0000

    sorted

---
+++ Assets/Scripts/Building/DoorScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim.ResetTrigger("OpenDoor");
+    }
+
+    public void OpenDoor() {
+        anim.SetTrigger("OpenDoor");
+    }
+
+    public void CloseDoor() {
+        anim.ResetTrigger("OpenDoor");
+        anim.enabled = true;
+    }
+
+    void pauseAnimationEvent() {
+        anim.enabled = false;
+    }
+}
