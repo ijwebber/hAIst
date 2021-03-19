3add5e4a7d9aedeae025f0142dbed11f4fd53a2b
williamwpeacock <47394604+williamwpeacock@users.noreply.github.com>
2021-03-19 10:54:25 +0000

    doorlist

--- Assets/Scripts/Building/DoorHandler.cs
+++ Assets/Scripts/Building/DoorHandler.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class DoorHandler : MonoBehaviour
+public class DoorHandler : MonoBehaviourPun
 {
 
     public PressButton button;
+    private List<int> l = new List<int>();
 
     // Start is called before the first frame update
     public DoorScript doorleft, doorright;
@@ -13,7 +15,15 @@ public class DoorHandler : MonoBehaviour
         if (other.gameObject.name == "Timmy" && button.done) {
             StartCoroutine(openDoor(doorleft));
             StartCoroutine(openDoor(doorright));
+
+            this.gameObject.GetComponent<PhotonView>().RPC("Enter", RpcTarget.Others);
         }
+    }
+
+    [PunRPC]
+    void Enter()
+    {
+        l.Add(1);
     }
 
     IEnumerator openDoor(DoorScript door) {
@@ -22,6 +32,21 @@ public class DoorHandler : MonoBehaviour
     }
     void OnTriggerExit(Collider other) {
         if (other.gameObject.name == "Timmy" && button.done) {
+
+            this.gameObject.GetComponent<PhotonView>().RPC("Exit", RpcTarget.Others);
+        }
+    }
+
+    [PunRPC]
+    void Exit()
+    {
+        if (l.Count != 0)
+        {
+            l.RemoveAt(0);
+        }
+
+        if (l.Count == 0)
+        {
             doorleft.CloseDoor();
             doorright.CloseDoor();
         }
