857660d755b2c12d8f4511d0ffa85533a3016227
Josh Felmeden <josh.felmeden@outlook.com>
2021-03-19 09:27:23 +0000

    resurrected will's files

--- Assets/Scripts/Tasks (Canvas)/KeycodeTask.cs
+++ Assets/Scripts/Tasks (Canvas)/KeycodeTask.cs
@@ -1,27 +1,26 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
 
 public class KeycodeTask : MonoBehaviour
 {
-    public Text _cardCode;
     public Text _inputCode;
     public int codeLength = 5;
     public float codeReset = 0.5f;  // code reset time in seconds
+    public PhotonView player;
     private bool isReset = false;
     public bool codeCorrect = false; // This code should be attached to the keycode task object in the canvas
 
-    private void OnEnable() {       // when the UI is active, do the following
-        string code = string.Empty;
+    public int keypadID = 0;
 
+    void Update() {
+
+    }
+    private void OnEnable()
+    {       // when the UI is active, do the following
         codeCorrect = false;
-
-        for(int i = 0; i < codeLength; i++){            // create a random code
-            code += Random.Range(1,10);
-        }
-
-        _cardCode.text = code;                          // input text
-        _inputCode.text = string.Empty;   
+        _inputCode.text = string.Empty;
     }
 
 
@@ -32,18 +31,31 @@ public class KeycodeTask : MonoBehaviour
 
         _inputCode.text += num;             // set inputcode box to what ever buttom input it is
 
-        if(_inputCode.text == _cardCode.text){
-            _inputCode.text = "Correct";
-            //insert bool value to say successful if code was correct
-            StartCoroutine(ResetCode());
-            codeCorrect=true;
-        }
+        KeyPad[] keypads = GameObject.FindObjectsOfType<KeyPad>();
 
-        else if(_inputCode.text.Length >= codeLength){
+        if (_inputCode.text.Length == codeLength)
+        {
+            foreach (KeyPad keypad in keypads)
+            {
+                if (keypad.id == keypadID && _inputCode.text == keypad.code)
+                {
+                    // Debug.Log("code submitted: " + _inputCode.text);
+                    _inputCode.text = "Correct";
+                    keypad.GetComponent<PhotonView>().RPC("updateKeyCode", RpcTarget.Others, keypadID);
+                    //insert bool value to say successful if code was correct
+                    StartCoroutine(ResetCode());
+                    keypad.codeCorrect = true;
+                    codeCorrect = true;
+                }
+            }
+        }
+        else if (_inputCode.text.Length >= codeLength)
+        {
             _inputCode.text = "Failed";
             StartCoroutine(ResetCode());
         }
     }
+
 
     private IEnumerator ResetCode(){  // reset the text inputs
         isReset = true;
---
+++ Assets/Scripts/Tasks (Canvas)/WireTask.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class WireTask : MonoBehaviour
+{
+    public bool complete;
+    //public int[] input;
+
+    public int wiresID = 0;
+
+    public void OnEnable()
+    {
+        complete = false;
+        //input = new int[4];
+    }
+
+    public void ButtonClick(int n)
+    {
+        Wires[] wiress = GameObject.FindObjectsOfType<Wires>();
+
+        foreach (Wires wires in wiress)
+        {
+            if (wires.id == wiresID)
+            {
+                wires.GetComponent<PhotonView>().RPC("updateWires", RpcTarget.Others, wiresID, ( wires.wire == n ));
+                wires.complete = true;
+                wires.correct = ( wires.wire == n );
+
+                complete = true;
+            }
+        }
+    }
+}
