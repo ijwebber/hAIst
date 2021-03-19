ae46907654cb9ba4707291f7a55a438020744ccd
Josh Felmeden <josh.felmeden@outlook.com>
2021-03-19 12:04:45 +0000

    metal door working with code

--- Assets/Scripts/Building/DoorHandlerKey.cs
+++ Assets/Scripts/Building/DoorHandlerKey.cs
@@ -7,14 +7,29 @@ public class DoorHandlerKey : MonoBehaviourPun
 {
 
     public KeyPad keyPad;
+    bool opened = false;
     private List<int> l = new List<int>();
 
     // Start is called before the first frame update
     public DoorScript doorleft, doorright;
     void OnTriggerEnter(Collider other) {
-        if (other.gameObject.name == "Timmy" && keyPad.codeCorrect) {
+        if (other.gameObject.name == "Timmy" && keyPad.codeCorrect && !opened) {
+            this.gameObject.GetComponent<PhotonView>().RPC("EnterMetal", RpcTarget.MasterClient);
+        }
+    }
+
+    [PunRPC]
+    void EnterMetalDone() {
+        opened = true;
+    }
+
+    [PunRPC]
+    void EnterMetal() {
+        if (!opened) {
             StartCoroutine(openDoor(doorleft));
             StartCoroutine(openDoor(doorright));
+            opened = true;
+            this.gameObject.GetComponent<PhotonView>().RPC("EnterMetalDone", RpcTarget.Others);
         }
     }
 
