9d985ce0511ea2dd24a34cf70a44a404700f149a
Alex <brownalexoscar@gmail.com>
2021-04-15 18:14:36 +0300

    ability to choose upgrades with tooltips

---
+++ Assets/TooltipTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public string content;
+    public string header;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        TooltipSystem.Show(content,header);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        TooltipSystem.Hide();
+    }
+
+    
+}
--- Assets/UpgradeController.cs
+++ Assets/UpgradeController.cs
@@ -56,26 +56,42 @@ public class UpgradeController : MonoBehaviour
                 GameObject obj = (GameObject)Instantiate(UpgradeImagePrefab2, UpgradePanel.transform);
 
                 obj.transform.GetChild(0).GetComponent<Image>().sprite = speed_boots;
+                obj.transform.GetChild(0).GetComponent<TooltipTrigger>().header = "speedy boots";
+                obj.transform.GetChild(0).GetComponent<TooltipTrigger>().content = "20% increased movement speed.";
+                obj.transform.GetChild(1).gameObject.SetActive(false);
+
             }
             else if (kvp.Key.Equals("shield") & kvp.Value > 0)
             {
                 GameObject obj = (GameObject)Instantiate(UpgradeImagePrefab2, UpgradePanelConsumables.transform);
                 obj.transform.GetChild(0).GetComponent<Image>().sprite = shield;
+                obj.transform.GetChild(0).GetComponent<TooltipTrigger>().header = "shield";
+                obj.transform.GetChild(0).GetComponent<TooltipTrigger>().content = "a shield that blocks your first knock-down.";
             }
             else if (kvp.Key.Equals("vision") & kvp.Value > 0)
             {
                 GameObject obj = (GameObject)Instantiate(UpgradeImagePrefab2, UpgradePanel.transform);
                 obj.transform.GetChild(0).GetComponent<Image>().sprite = vision;
+                obj.transform.GetChild(0).GetComponent<TooltipTrigger>().header = "super glasses";
+                obj.transform.GetChild(0).GetComponent<TooltipTrigger>().content = "20% increased vision radius.";
+                obj.transform.GetChild(1).gameObject.SetActive(false);
+
             }
             else if (kvp.Key.Equals("self_revive") & kvp.Value > 0)
             {
                 GameObject obj = (GameObject)Instantiate(UpgradeImagePrefab2, UpgradePanelConsumables.transform);
                 obj.transform.GetChild(0).GetComponent<Image>().sprite = self_revive;
+                obj.transform.GetChild(0).GetComponent<TooltipTrigger>().header = "self revive";
+                obj.transform.GetChild(0).GetComponent<TooltipTrigger>().content = "ability to revive yourself once after being knocked down.";
             }
             else if (kvp.Key.Equals("fast_hands") & kvp.Value > 0)
             {
                 GameObject obj = (GameObject)Instantiate(UpgradeImagePrefab2, UpgradePanel.transform);
                 obj.transform.GetChild(0).GetComponent<Image>().sprite = fast_hands;
+                obj.transform.GetChild(0).GetComponent<TooltipTrigger>().header = "fast hands";
+                obj.transform.GetChild(0).GetComponent<TooltipTrigger>().content = "20% decreased time to steal.";
+                obj.transform.GetChild(1).gameObject.SetActive(false);
+
             }
 
         }
