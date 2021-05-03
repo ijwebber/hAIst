71850d6dc4e984b147b0081ace8ba8f0a18270f9
will <william.wpeacock@gmail.com>
2021-05-03 18:21:57 +0100

    updated disable nearest laser

--- Assets/Scripts/Building/LaserController.cs
+++ Assets/Scripts/Building/LaserController.cs
@@ -5,16 +5,44 @@ public class LaserController : MonoBehaviour
 {
     public void DisableNearestLaser(Vector3 pos)
     {
-        Laser closest = null;
+        Laser closestLaser = null;
         Laser[] lasers = GameObject.FindObjectsOfType<Laser>();
         foreach (Laser laser in lasers)
         {
-            if (closest == null || Vector3.Distance(laser.GetComponent<Transform>().position, pos) < Vector3.Distance(closest.GetComponent<Transform>().position, pos))
+            if (closestLaser == null || Vector3.Distance(laser.GetComponent<Transform>().position, pos) < Vector3.Distance(closestLaser.GetComponent<Transform>().position, pos))
             {
-                closest = laser;
+                closestLaser = laser;
             }
         }
 
-        closest.GetComponent<PhotonView>().RPC("disableLaser", RpcTarget.All);
+        LaserDown closestLaserDown = null;
+        LaserDown[] laserDowns = GameObject.FindObjectsOfType<LaserDown>();
+        foreach (LaserDown laserDown in laserDowns)
+        {
+            if (closestLaserDown == null || Vector3.Distance(laserDown.GetComponent<Transform>().position, pos) < Vector3.Distance(closestLaserDown.GetComponent<Transform>().position, pos))
+            {
+                closestLaserDown = laserDown;
+            }
+        }
+
+        if (closestLaser != null && closestLaserDown != null)
+        {
+            if (Vector3.Distance(closestLaser.GetComponent<Transform>().position, pos) < Vector3.Distance(closestLaserDown.GetComponent<Transform>().position, pos))
+            {
+                closestLaser.GetComponent<PhotonView>().RPC("disableLaser", RpcTarget.All);
+            }
+            else
+            {
+                closestLaserDown.GetComponent<PhotonView>().RPC("disableLaser", RpcTarget.All);
+            }
+        }
+        else if (closestLaser != null)
+        {
+            closestLaser.GetComponent<PhotonView>().RPC("disableLaser", RpcTarget.All);
+        }
+        else if (closestLaserDown != null)
+        {
+            closestLaserDown.GetComponent<PhotonView>().RPC("disableLaser", RpcTarget.All);
+        }
     }
 }
