6acf52b2383c4ab1e2fd6f6199b5bc1dc6246037
Josh Felmeden <josh.felmeden@outlook.com>
2021-05-07 11:54:04 +0100

    updated spotlight

--- Assets/Scripts/Building/SpotLightHitbox.cs
+++ Assets/Scripts/Building/SpotLightHitbox.cs
@@ -9,7 +9,14 @@ public class SpotLightHitbox : MonoBehaviour
     [SerializeField] private GameObject SpotLight;
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.layer == 9) {
-            guardController.GetClosestGuard(SpotLight.transform.position.x, ) (SpotLight, other.gameObject);
+            SpotLight.GetComponent<Light>().color = Color.red;
+            guardController.GetClosestGuard(SpotLight.transform.position.x, SpotLight.transform.position.y, SpotLight.transform.position.z);
+        }
+    }
+    void OnTriggerLeave(Collider other) {
+        if (other.gameObject.layer == 9) {
+            SpotLight.GetComponent<Light>().color = Color.white;
+            guardController.GetClosestGuard(SpotLight.transform.position.x, SpotLight.transform.position.y, SpotLight.transform.position.z);
         }
     }
 }
