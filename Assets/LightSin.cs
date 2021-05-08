5a7024d899105d3ae562fe52041578a02bd8000c
Daanial <Daanial74@gmail.com>
2021-05-08 15:41:14 +0100

    c4

---
+++ Assets/LightSin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSin : MonoBehaviour
+{
+
+    Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        StartCoroutine(onOff());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator onOff()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.3f);
+            if (light.intensity == 1f)
+            {
+                light.intensity = 0;
+            }
+            else light.intensity = 1;
+            
+        }
+    }
+}
