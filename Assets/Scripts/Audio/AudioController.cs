cd41d15df43f0ae7525ca53c4e2b21369a3b1f31
Isaac Webber <isaacjwebber@gmail.com>
2021-05-10 21:20:38 +0100

    added intense and stealth to the art level

--- Assets/Scripts/Audio/AudioController.cs
+++ Assets/Scripts/Audio/AudioController.cs
@@ -2,12 +2,27 @@
 
 public class AudioController : MonoBehaviour
 {
-    
-    void Start() {
+    [SerializeField] private AudioSource mainPlayer;
+    [SerializeField] private AudioSource addPlayer;
 
-    }
+    [SerializeField] private AudioClip intenseMain;
+    [SerializeField] private AudioClip intenseAdd;
+
+    [SerializeField] private AudioClip intenseMainIntro;
+    [SerializeField] private AudioClip intenseAddIntro;
+    
 
     public void PlayIntenseTheme() {
+        mainPlayer.clip = intenseMain;
+        addPlayer.clip = intenseAdd;
+
+        if (!mainPlayer.isPlaying) {
+            mainPlayer.Play();
+        }
+
+        if (!addPlayer.isPlaying) {
+            addPlayer.Play();
+        }
     }
 
     /*public void PlayLowValue() {
