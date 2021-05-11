e23b1cca1cfb8dd78e8e3cecddb3922f61d26114
Isaac Webber <isaacjwebber@gmail.com>
2021-05-11 10:49:49 +0100

    josh's fix for music

--- Assets/Scripts/Audio/AudioController.cs
+++ Assets/Scripts/Audio/AudioController.cs
@@ -19,17 +19,29 @@ public class AudioController : MonoBehaviour
     [SerializeField] private AudioClip lowValueSFX;
     [SerializeField] private AudioClip highValueSFX;
 
-    public void PlayIntenseTheme() {
-        mainPlayer.Pause();
-        addPlayer.Pause();
-        mainPlayer.clip = intenseMain;
-        addPlayer.clip = intenseAdd;
+    private bool isNotNull;
 
-        double startTime = AudioSettings.dspTime + 0.2;
-        double introDuration = (double) introPlayer.clip.samples / introPlayer.clip.frequency;
-        introPlayer.PlayScheduled(startTime);
-        mainPlayer.PlayScheduled(introDuration + startTime);
-        addPlayer.PlayScheduled(introDuration + startTime);
+    void Start() {
+        if (intenseMain == null) {
+            isNotNull = true;
+        } else {
+            isNotNull = false;
+        }
+    } 
+
+    public void PlayIntenseTheme() {
+        if (isNotNull) {
+            mainPlayer.Pause();
+            addPlayer.Pause();
+            mainPlayer.clip = intenseMain;
+            addPlayer.clip = intenseAdd;
+
+            double startTime = AudioSettings.dspTime + 0.2;
+            double introDuration = (double) introPlayer.clip.samples / introPlayer.clip.frequency;
+            introPlayer.PlayScheduled(startTime);
+            mainPlayer.PlayScheduled(introDuration + startTime);
+            addPlayer.PlayScheduled(introDuration + startTime);
+        }
     }
 
     public void EnableAdditional() {
@@ -37,14 +49,16 @@ public class AudioController : MonoBehaviour
     }
 
     private void PlaySFX(AudioClip clip) {
-        if (alternate) {
-            sfxPlayer1.clip = clip;
-            sfxPlayer1.Play();
-        } else {
-            sfxPlayer2.clip = clip;
-            sfxPlayer2.Play();
+        if (isNotNull) {
+            if (alternate) {
+                sfxPlayer1.clip = clip;
+                sfxPlayer1.Play();
+            } else {
+                sfxPlayer2.clip = clip;
+                sfxPlayer2.Play();
+            }
+            alternate = !alternate;
         }
-        alternate = !alternate;
     }
 
     public void PlayLowValue() {
