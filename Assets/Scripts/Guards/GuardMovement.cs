b72b8adc8537b61d1368c5872af89c8127a00952
Josh Felmeden <josh.felmeden@outlook.com>
2021-05-11 15:36:02 +0100

    bro

--- Assets/Scripts/Guards/GuardMovement.cs
+++ Assets/Scripts/Guards/GuardMovement.cs
@@ -107,6 +107,7 @@ public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
             Hashtable playerHash = new Hashtable();
             int specCount = (int) PhotonNetwork.LocalPlayer.CustomProperties["specialStolen"];
             playerHash.Add("specialStolen", specCount + specials.Count);
+            Debug.LogError("Stole back. Spec count " + specCount + " -> " + (specCount+specials.Count))
             PhotonNetwork.LocalPlayer.SetCustomProperties(playerHash);
             gameController.gameState++;
             gameController.regress = false;
@@ -198,7 +199,6 @@ public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
                                 PhotonNetwork.LocalPlayer.SetCustomProperties(specHash);
                                 foreach (var spec in playerController.Specials)
                                 {
-                                    
                                     serializedObjects += spec.name + ",";
                                     spec.GetComponent<CollectableItem>().stolen = false; // point to guard;
                                     spec.GetComponent<CollectableItem>().guardPoint = this.gameObject; // point to guard;
