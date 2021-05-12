9b135c57fbb773bc5109078eda23c0a7f2b83e40
Josh Felmeden <josh.felmeden@outlook.com>
2021-05-12 18:19:13 +0100

    added ping indicator

---
+++ Assets/Scripts/UI/PingIndicator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using TMPro;
+
+public class PingIndicator : MonoBehaviourPun
+{
+    [SerializeField] private Sprite pingGood, pingOkay, pingPoor, pingAwful;
+    [SerializeField] private Image pingIndicator;
+    [SerializeField] private TextMeshProUGUI pingText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int ping = PhotonNetwork.GetPing();
+        pingText.text = ping.ToString();
+        if (ping < 100) {
+            pingIndicator.sprite = pingGood;
+        } else if (ping < 150) {
+            pingIndicator.sprite = pingOkay;
+        } else if (ping < 200) {
+            pingIndicator.sprite = pingPoor;
+        } else {
+            pingIndicator.sprite = pingAwful;
+        }
+    }
+}
