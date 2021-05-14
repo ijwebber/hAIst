6b9d24260017251c935cd4d57892cf67653c5bc1
Isaac Webber <isaacjwebber@gmail.com>
2021-05-14 14:59:02 +0100

    can build again

--- Assets/Scripts/Sound/SoundController.cs
+++ Assets/Scripts/Sound/SoundController.cs
@@ -14,6 +14,11 @@ public class SoundController : MonoBehaviourPun
     public Grid grid;
     public GuardController localSoundGrid;
     private bool soundEnabled = false;
+    // public GuardMovement guardController;
+    public GameObject gridContainer;
+    public int maxVolume;
+    float timeElapsed;
+    
         void Awake()
         {
 #if UNITY_WEBGL && !UNITY_EDITOR
