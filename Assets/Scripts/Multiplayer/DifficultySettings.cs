5d44bdb8d357cc8790abc48325a5933571d9555b
Josh Felmeden <josh.felmeden@outlook.com>
2021-05-16 23:01:19 +0100

    ligma

--- Assets/Scripts/Multiplayer/DifficultySettings.cs
+++ Assets/Scripts/Multiplayer/DifficultySettings.cs
@@ -16,7 +16,7 @@ public class DifficultySettings : MonoBehaviourPun
         defaultSettings.Add("movespeedSetting", (float)(moveSlider.value/2));
         defaultSettings.Add("radiusSetting", (int)radiusSlider.value);
         defaultSettings.Add("timeSetting", (int)timerSlider.value);
-        defaultSettings.Add("scoreMultipler", (float)((moveSlider.value - 7)*2 + (radiusSlider.value-10)*2 + (4 - timerSlider.value)*2));
+        defaultSettings.Add("scoreMultiplier", (float)((moveSlider.value - 7)*5 + (radiusSlider.value-10)*2 + (4 - timerSlider.value)*2));
         PhotonNetwork.CurrentRoom.SetCustomProperties(defaultSettings);
     }
     public void SetupDifficulties() {
@@ -32,28 +32,28 @@ public class DifficultySettings : MonoBehaviourPun
         moveSliderText.text = (moveSpeedSetting).ToString();
         radiusSliderText.text = radiusSetting.ToString();
         timerSliderText.text = timeSetting.ToString();
-        moveMultiplier.text = (2*(moveSpeedSetting*2 - 7)).ToString() + "%";
+        moveMultiplier.text = (5*(moveSpeedSetting*2 - 7)).ToString() + "%";
         radiusMultiplier.text = ((radiusSetting-10)*2).ToString() + "%";
         timerMultiplier.text = ((4-timeSetting)*2).ToString() + "%";
-        totalMultiplier.text = "Total: " + ((moveSlider.value - 7)*2 + (radiusSlider.value-10)*2 + (4 - timerSlider.value)*2).ToString() + "%";
+        totalMultiplier.text = "Total: " + ((moveSlider.value - 7)*5 + (radiusSlider.value-10)*2 + (4 - timerSlider.value)*2).ToString() + "%";
     }
 
     public void moveSliderUpdate() {
         moveSliderText.text = (moveSlider.value/2).ToString();
         moveMultiplier.text = ((moveSlider.value-7)*2).ToString() + "%";
-        totalMultiplier.text = "Total: " + ((moveSlider.value - 7)*2 + (radiusSlider.value-10)*2 + (4 - timerSlider.value)*2).ToString() + "%";
+        totalMultiplier.text = "Total: " + ((moveSlider.value - 7)*5 + (radiusSlider.value-10)*2 + (4 - timerSlider.value)*2).ToString() + "%";
     }
 
     public void radiusSliderUpdate() {
         radiusSliderText.text = radiusSlider.value.ToString();
         radiusMultiplier.text = ((radiusSlider.value - 10)*2).ToString() + "%";
-        totalMultiplier.text = "Total: " + ((moveSlider.value - 7)*2 + (radiusSlider.value-10)*2 + (4 - timerSlider.value)*2).ToString() + "%";
+        totalMultiplier.text = "Total: " + ((moveSlider.value - 7)*5 + (radiusSlider.value-10)*2 + (4 - timerSlider.value)*2).ToString() + "%";
     }
 
     public void timerSliderUpdate() {
         timerSliderText.text = timerSlider.value.ToString();
         timerMultiplier.text = ((4 - timerSlider.value)*2).ToString() + "%";
-        totalMultiplier.text = "Total: " + ((moveSlider.value - 7)*2 + (radiusSlider.value-10)*2 + (4 - timerSlider.value)*2).ToString() + "%";
+        totalMultiplier.text = "Total: " + ((moveSlider.value - 7)*5 + (radiusSlider.value-10)*2 + (4 - timerSlider.value)*2).ToString() + "%";
     }
 
     public void btnDefaultClick() {
