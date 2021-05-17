740ebb27608771e2fde31facd88bbf1d2f358ab6
Daanial <Daanial74@gmail.com>
2021-05-17 12:45:02 +0100

    Removed change

--- Assets/Scripts/Guards/GuardMovement.cs
+++ Assets/Scripts/Guards/GuardMovement.cs
@@ -408,12 +408,9 @@ public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     IEnumerator disableForTime(float disableTime)
     {
         // if (this.specials.Count)
-        while (CameraSystem.Instance.isCutSceneHappening)
-        {
-            yield return new WaitForSeconds(0.1f);
-            
-        }
+        
         yield return new WaitForSeconds(disableTime);
+
         guardDisabled = false;
         if (PhotonNetwork.IsMasterClient) {
             this.state = State.normal;
